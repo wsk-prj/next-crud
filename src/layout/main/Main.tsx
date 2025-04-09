073914434331402a2db381cac96cfc6709d2ebaa
wsk0715 <leindey@gmail.com>
2025-04-09 11:35:47 +0900

    :bug:fix: 새로고침 시 화면 렌더링 문제 수정

--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -2,15 +2,25 @@
 
 import { useAuthStore } from "@/hooks/useAuthStore";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Main = ({ children }: { children: React.ReactNode }): React.ReactNode => {
   const { loggedIn } = useAuthStore();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (loggedIn) {
+      setIsLoading(false);
+    }
+  }, [loggedIn]);
 
   return (
     <main id="main" className="px-8 py-4 h-5/6 flex flex-col gap-y-4">
       <section id="navbar">
-        <nav className="flex items-center justify-center gap-x-2">
-          {loggedIn ? (
+        <nav className="flex h-4 items-center justify-center gap-x-2">
+          {isLoading ? (
+            ""
+          ) : loggedIn ? (
             <>
               <Link href="/mypage">!회원정보</Link>
               <Link href="/board">!게시판</Link>
